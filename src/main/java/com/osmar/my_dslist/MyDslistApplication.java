0fa351bf86b50ffb2c1e49945b73a3393682bb6f
Osmar-Araujo <araujooa@gmail.com>
2025-08-18 11:06:23 -0300

    project created

---
+++ src/main/java/com/osmar/my_dslist/MyDslistApplication.java
@@ -0,0 +1,13 @@
+package com.osmar.my_dslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDslistApplication.class, args);
+	}
+
+}
