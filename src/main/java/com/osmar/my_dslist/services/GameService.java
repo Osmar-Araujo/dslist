ada6220cac3475c46b8204110ae69082bc6c5295
Osmar-Araujo <araujooa@gmail.com>
2025-08-19 11:45:11 -0300

    Buscando game por id

--- src/main/java/com/osmar/my_dslist/services/GameService.java
+++ src/main/java/com/osmar/my_dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.osmar.my_dslist.dto.GameDTO;
 import com.osmar.my_dslist.dto.GameMinDTO;
 import com.osmar.my_dslist.entities.Game;
 import com.osmar.my_dslist.repositories.GameRepository;
@@ -15,6 +17,14 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
+	public GameDTO findById (Long gameId) {
+		Game result = gameRepository.findById(gameId).get();
+		GameDTO dto = new GameDTO(result);
+		return dto;
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
 		 
